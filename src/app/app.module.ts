e35c9b5affea2e20604cb5fbea1e3b1bb5cebbd1
Uziano <lucianoperaltadefazio@gmail.com>
2022-08-23 12:30:45 -0300

    Parte 3 terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
+import { LogosComponent } from './componentes/logos/logos.component';
+import { RedesComponent } from './componentes/redes/redes.component';
+import { BannerComponent } from './componentes/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogosComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
